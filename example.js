4094d9f6b7e3baef0a113ce07f668d608376353e
Alex Langberg <mail@alexlangberg.dk>
2015-09-20 22:29:26 +0200

    Update package name.

--- example.js
+++ example.js
@@ -6,8 +6,8 @@ var ss = sitesampler('settings.default.json');
 ss.start();
 
 ss.on('results', function(data) {
-    console.log(data);
-  });
+  console.log(data);
+});
 
 ss.on('error', function(error) {
   console.log(error);
