7ba81eef2ee17b2cd4527af7e752af552f295efe
Alex Langberg <mail@alexlangberg.dk>
2015-10-12 22:43:15 +0200

    Change example.

--- example.js
+++ example.js
@@ -3,8 +3,8 @@
 var sitesampler = require('./');
 var ss = sitesampler('sitesampler.default.json');
 
-ss.on('response', function(response) {
-  console.log(response);
+ss.on('response', function(result) {
+  console.log(result);
 });
 
 ss.on('error', function(error) {
